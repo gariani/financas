20e024e4fa736ff68c56342d868affdb834f9c3c
Daniel Gariani Rafael <danielgrafael@gmail.com>
2017-07-05 22:29:48 -0300

    Teste Heroku

--- Response.py
+++ Response.py
@@ -1,2 +1,16 @@
-import json
-import falcon
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hello world")
+
+def make_app():
+    return tornado.web.Application([(r"/", MainHandler), ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
