6f59b4d8782bbedfb4de89804aef0a5dcc6da92d
Daniel Gariani Rafael <danielgrafael@gmail.com>
2017-07-07 00:14:32 -0300

    Ajustanto mock para retornar json

--- Response.py
+++ Response.py
@@ -4,24 +4,48 @@ import tornado.ioloop
 import tornado.web
 import json
 
+class Mock():
+    def mock(self, id=1):
+        dic = {}
+        dic["id"] = id
+        dic["descricaoPrevisto"] = "Alimentação"
+        dic["valorPrevisto"] = 200.00
+        dic["realizados"] = "realizados1"
+        dic["totalRealizado"] = 10.0
+        dic["saldo"] = 190.00
+        return dic
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         try:
-            response = {'status':'sucesso','resposta':'oi marcel!'}
+            _mock = Mock()
+            _valor = _mock.mock()
+            response = _valor
             kk = tornado.escape.json_encode(response)
-            kk = wrap_callback(self, kk)
             self.write(kk)
-        except Exception e:
-            self.write(json.dumps({'status':'fail','error': "Error: %s" % format_exec()}))
-         
+        except Exception() as e: 
+            self.write(json.dumps(
+                {'status': 'fail', 'error': "Error: %s" % format(e)}))
 
+
+class IdHandler(tornado.web.RequestHandler):
+    
+    def get(self, *args, **kwargs):
+        _id = args[0]
+        _mock = Mock()
+        _valor = _mock.mock(_id)
+        self.write(_valor)
+ 
 def make_app():
-    application = tornado.web.Application([(r"/", MainHandler), ])
+    application = tornado.web.Application(
+        [(r"/", MainHandler),
+         (r"/id/(\d+)$", IdHandler), ], autoreload=True)
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.current().start()
 
+
 if __name__ == "__main__":
-    make_app()    
-    
+    make_app()
